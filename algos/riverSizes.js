44e0df5c24ea7e16928d90d50f3d9f6ba3e78e39
Colin Folta <colin.folta@gmail.com>
2020-02-19 13:33:33 -0600

    add river sizes algo

---
+++ algos/riverSizes.js
@@ -0,0 +1,84 @@
+/* 
+    Takes an argument of a 2D matrix of 0 or 1. 0 = land, 1 = river.
+    Returns the lengths of all contiguous rivers in the array.
+    "contiguous" means adjacent vertically or horizontally, but not diagonally.
+
+    Overall strategy is to raster through the array, then if finding a river, begin "following" it.
+    We keep track of what has been visited (a separate array of equal size) and skip any visited cell.
+*/
+
+/**
+ * 
+ * @param {array} matrix 
+ */
+function riverSizes(matrix) {
+    /* ----------------------- */
+    // VALIDATION
+    if (!Array.isArray(matrix)|| !matrix.length || !matrix[0].length) throw new TypeError('not a 2D array');
+
+	const sizes = [];
+	// initialize visited
+	const visited = [...Array(matrix.length)].map(
+		() => Array(matrix[0].length).fill(false)
+    );
+    
+    /* ----------------------- */
+    // ALGO
+    
+    // start rastering through matrix
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			// if already visited, skip calculations
+			if (visited[i][j]) continue;
+			startTraversing(i, j, matrix, visited, sizes);
+		}
+	}
+	return sizes;
+}
+
+function startTraversing(i, j, matrix, visited, sizes) {
+	let newSize = 0;
+	const nodesToExplore = [[i, j]];
+	while (nodesToExplore.length) {
+		const currentNode = nodesToExplore.pop();	// pulls end coords off array
+		const x = currentNode[0];
+		const y = currentNode[1];
+		// if already visited, dont perform calcs
+        if (visited[x][y]) continue;
+        
+        visited[x][y] = true; // mark as visited
+
+        // if land, stop following this direction
+        if (matrix[x][y] === 0) continue;
+        
+		// if river, increment size and add more nodes to inspect
+		newSize += 1;
+		const unvisitedNeighbors = getUnvisitedNeighbors(x, y, matrix, visited);
+		nodesToExplore.push(...unvisitedNeighbors);
+	}
+	if (newSize > 0) sizes.push(newSize);
+}
+
+function getUnvisitedNeighbors(i, j, matrix, visited) {
+	const unvisitedNeighbors = [];
+	// add node above
+	if (i > 0 && !visited[i - 1][j]) {
+		unvisitedNeighbors.push([i - 1, j]);
+	}
+	// add node below
+	if (i < matrix.length - 1 && !visited[i + 1][j]) {
+		unvisitedNeighbors.push([i + 1, j]);
+	}
+	// add node to left
+	if (j > 0 && !visited[i][j - 1]) {
+		unvisitedNeighbors.push([i, j - 1]);
+	}
+	// add node to right
+	if (j < matrix[i].length - 1 && !visited[i][j + 1]) {
+		unvisitedNeighbors.push([i, j + 1]);
+	}
+	return unvisitedNeighbors;
+}
+
+exports.riverSizes = riverSizes;
+
---
+++ test/riverSizes.js
@@ -0,0 +1,80 @@
+const chai = require('chai');
+const program = require('../algos/riverSizes');
+
+describe('River Sizes', function() {
+    it('errors if arg is not an array', function() {
+        chai.expect(() => program.riverSizes('abc')).to.throw();
+    })
+
+    it('errors if arg is an empty array', function() {
+        chai.expect(() => program.riverSizes( [] )).to.throw();
+    })
+
+    it('errors if arg is not a 2D array', function() {
+        chai.expect(() => program.riverSizes( [1] )).to.throw();
+    })
+
+    it('1x1 land', function() {
+        const testInput = [[0]];
+        const expected = [];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('1x1 river', function() {
+        const testInput = [[1]];
+        const expected = [1];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('1x10', function() {
+        const testInput = [[0, 1, 1, 1, 0, 0, 0, 1, 1, 0]];
+        const expected = [2, 3];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('4x4', function() {
+        const testInput = [
+            [1, 0, 1, 1],
+            [1, 0, 0, 1],
+            [1, 1, 0, 0],
+            [0, 1, 0, 1],
+        ];
+        const expected = [1, 3, 5];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('4x4 donut', function() {
+        const testInput = [
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+        ];
+        const expected = [12];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('5x5', function() {
+        const testInput = [
+            [0, 1, 1, 1, 1],
+            [0, 0, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [1, 1, 0, 1, 0],
+            [0, 0, 1, 1, 1],
+        ];
+        const expected = [4, 4, 5];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+
+    it('5x5 all river', function() {
+        const testInput = [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+        ];
+        const expected = [25];
+        chai.expect(program.riverSizes(testInput).sort((a,b) => a - b)).to.deep.equal(expected);
+    });
+})
